0e3e8e564d69be3a04454c3f10e9595e800c724e
OskrEdu <121064188+OskrEdu@users.noreply.github.com>
2023-10-27 21:14:39 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,30 +5,49 @@ import ota  # Asegúrate de tener este módulo
 
 
 # Constantes para la versión del firmware, la URL de actualización y el pin del LED
-FIRMWARE_VERSION = 1.1  # Debe ser un float
+FIRMWARE_VERSION = 1.0  # Debe ser un float
 UPDATE_URL = "https://nachobeta07.github.io/firmware_microPython.json"
-LED_PIN = 17  # GPIO para el LED
+LED_PIN = 18  # GPIO para el LED
 
 # Configuración inicial de los dispositivos/actuadores (por ejemplo, LED)
 # NOTA: Los usuarios deben modificar estas líneas según sea necesario
 pin_device = LED_PIN  # Usamos la constante LED_PIN para el pin GPIO del LED
 device = machine.Pin(pin_device, machine.Pin.OUT)
+def led_blinking_control():
+    """
+    Controla el parpadeo del LED. Si 'stop_blinking' es True, detiene el parpadeo.
+    """
+    global stop_blinking
+    while not stop_blinking:
+        led.value(not led.value())  # Cambia el estado del LED
+        time.sleep(0.5)  # Parpadeo cada 0.5 segundos
+
 def device_control_logic():
     """
-    Lógica de control para dispositivos/actuadores (como relés, LEDs, etc.)
+    Lógica de control para dispositivos/actuadores (como LEDs, relés, etc.).
 
     Los usuarios deben modificar esta función según sus necesidades específicas.
     """
-    while True:
-        # Encender el LED
-        print("Encendiendo el LED.")
-        device.value(True)  # Encender el LED
-        time.sleep(5)
+    global stop_blinking
+    try:
+        while True:
+            # Iniciar el parpadeo del LED
+            stop_blinking = False
+            _thread.start_new_thread(led_blinking_control, ())
 
-        # Apagar el LED
-        print("Apagando el LED.")
-        device.value(False)  # Apagar el LED
-        time.sleep(5)
+            # Esperar 5 segundos
+            time.sleep(5)
+
+            # Detener el parpadeo del LED
+            stop_blinking = True
+            time.sleep(0.5)  # Asegurarse de que el parpadeo se detenga antes de continuar
+
+            # Apagar el LED
+            led.value(False)
+            time.sleep(1)  # Esperar 1 segundo
+    except KeyboardInterrupt:
+        stop_blinking = True  # Asegurarse de que el parpadeo se detenga antes de salir
+
 
 def ota_update_check():
     """
