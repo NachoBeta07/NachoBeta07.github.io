84cb027e252ae1ef03408be6b3ba87bf04e2179b
Miguel Paladines <109192521+NachoBeta07@users.noreply.github.com>
2023-11-19 19:10:16 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import _thread
 import ota  # Asegúrate de tener este módulo
 
 # Constantes para la versión del firmware y la URL de actualización
-FIRMWARE_VERSION = 1.0  # Debe ser un float
+FIRMWARE_VERSION = 1.1  # Debe ser un float
 UPDATE_URL = "https://nachobeta07.github.io/firmware_microPython.json"
 
 # Variables para el LED
-current_led_pin = 17  # GPIO para el LED actual
+current_led_pin = 18  # GPIO para el LED actual
 led = machine.Pin(current_led_pin, machine.Pin.OUT)
 
 # Control para el bucle de parpadeo del LED
@@ -19,43 +19,12 @@ def led_blinking_control():
     Controla el parpadeo del LED. Si 'stop_blinking' es True, detiene el parpadeo.
     """
     global stop_blinking
+    led.value(True)  # Enciende el LED inicialmente
     while not stop_blinking:
         led.value(not led.value())  # Cambia el estado del LED
         time.sleep(0.5)  # Parpadeo cada 0.5 segundos
+    led.value(False)  # Asegura que el LED se apague al detener el parpadeo
 
-def device_control_logic():
-    """
-    Lógica de control para dispositivos/actuadores (como LEDs, relés, etc.)
-    """
-    global stop_blinking
-    global current_led_pin
-    try:
-        while True:
-            # Encender el LED
-            led.value(True)
-
-            # Iniciar el parpadeo del LED
-            stop_blinking = False
-            _thread.start_new_thread(led_blinking_control, ())
-
-            # Esperar 5 segundos
-            time.sleep(5)
-
-            # Detener el parpadeo del LED
-            stop_blinking = True
-
-            # Apagar el LED
-            led.value(False)
-
-            # Cambiar el pin del LED (simplemente cambia este valor)
-            new_led_pin = 19  # Cambia este valor al nuevo pin que desees
-            led.init(machine.Pin(new_led_pin, machine.Pin.OUT))
-            current_led_pin = new_led_pin
-
-            # Esperar 1 segundo antes de repetir el ciclo
-            time.sleep(1)
-    except KeyboardInterrupt:
-        stop_blinking = True  # Asegurarse de que el parpadeo se detenga antes de salir
 
 def main():
     """
@@ -65,10 +34,12 @@ def main():
     global current_led_pin
 
      # Configura el pin antiguo del LED a bajo para apagarlo si es necesario
-    old_led_pin = 18
-    if current_led_pin != old_led_pin:  # Verifica si el pin actual es diferente al antiguo
-        old_led = machine.Pin(old_led_pin, machine.Pin.OUT)
-        old_led.value(False)  # Apaga el LED del pin antiguo
+   # Lista de pines del firmware anterior para apagarlos si es necesario
+    old_pins = [17]  # Asegúrate de actualizar esta lista según sea necesario
+    for pin_number in old_pins:
+        if current_led_pin != pin_number:  # Verifica si el pin actual es diferente al antiguo
+            old_led = machine.Pin(pin_number, machine.Pin.OUT)
+            old_led.value(False)  # Apaga el LED del pin antiguo
 
     # Iniciar el parpadeo del LED en un nuevo hilo
     _thread.start_new_thread(led_blinking_control, ())
